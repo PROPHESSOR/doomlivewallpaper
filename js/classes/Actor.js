a8975ebda4f4a8c7e5ffd1d482fc5c650ba96092
PROPHESSOR <xenteze@gmail.com>
2017-11-04 19:43:42 +0200

    Begining state machine

--- js/classes/Actor.js
+++ js/classes/Actor.js
@@ -1,9 +1,22 @@
 'use strict';
 
-class Actor{
-	constructor(){}
+class Actor {
+	constructor(sprite_folder = '', params = {}, spawn = []) {
+		this.sprite_folder = sprite_folder;
+		this._params = params;
+		this._spawn = spawn;
 
-	render(x,y){}//eslint-disable-line
+		this.states = [
+			[
+				null, // Sprite name (null = no sprite)
+				-1 // animation length (-1 = infinite)
+			]
+		];
+	}
+
+	render(x, y) {} //eslint-disable-line
+
+
 }
 
 module.exports = Actor;
