ecafe1a914fe51c3df8b953b9409c090152c18b9
PROPHESSOR <xenteze@gmail.com>
2017-12-23 00:34:14 +0200

    Документирование кода

--- js/classes/Actor.js
+++ js/classes/Actor.js
@@ -1,12 +1,32 @@
+/*
+ * Copyright (c) 2017 PROPHESSOR
+*/
+
 'use strict';
 
+/**
+ * @namespace Actor
+ * @class
+ */
+
 class Actor {
+
+	/**
+	 * @constructor
+	 * @memberof Actor
+	 * @param  {string} spriteFolder='' - Название папки со спрайтами актора
+	 * @param  {object} params={} - Параметры актора
+	 * @param  {array} spawn=[] - Координаты спавна
+	 * @param {number} spawn.0 - X-координата
+	 * @param {number} spawn.1 - Y-координата
+	 */
 	constructor(spriteFolder = '', params = {}, spawn = []) {
 		this.spriteFolder = spriteFolder;
 		this._params = params;
 		this._spawn = spawn;
 		this.element = Actor.createActorElement();
 		[this.x, this.y] = spawn;
+		[this.width, this.height] = [this.element.width, this.element.height];
 		this.direction = params.direction || 1;
 
 		this.sounds = params.sounds || {
@@ -47,18 +67,26 @@ class Actor {
 		// this.setSprite('bossa1');
 	}
 
+	/** Инициализировать состояния и запустить цикл (spawn)
+	 */
 	initStates() {
 		this.gotoState('spawn');
 		this.isGoto = false;
 		this.updateState(0);
 	}
 
+	/** Развернуть на 180°
+	 * @returns {number} Id направления
+	 */
 	reverseDirection() {
 		this.direction = Actor.reverseDirection(this.angle);
 
 		return this.direction;
 	}
 
+	/** Повернуться к
+	 * @param  {Actor} target - Цель
+	 */
 	turnTo(target) {
 		const data = Actor.calculateDirection(this, target);
 
@@ -66,16 +94,27 @@ class Actor {
 		this.angle = data.angle;
 	}
 
+	/** Отобразить элемент на экране (append)
+	 */
 	spawn() {
 		this.element.style.position = 'absolute';
 		$('#main').append(this.element);
 	}
 
+	/** Отрисовать элемент
+	 * @param  {number} x=this.x - X-координата
+	 * @param  {number} y=this.y - Y-координата
+	 */
 	render(x = this.x, y = this.y) {
 		this.element.style.left = `${x}px`;
 		this.element.style.top = `${y}px`;
 	}
 
+	/** Воспроизвести звук
+	 * @param  {string} sound - Имя файла без расширения (.ogg)
+	 * @param  {bool} loop=false - Циклично?
+	 * @returns {undefined} ...
+	 */
 	sound(sound, loop = false) { //TODO: Проверка на наличие кодека
 		if (!sound) {
 			return console.warn('Нет звука для воспроизвидения!');
@@ -88,6 +127,10 @@ class Actor {
 		player.play();
 	}
 
+	/** Искать состояние
+	 * @param  {string} name - Состояние
+	 * @returns {number} ID состояния в массиве
+	 */
 	findState(name) {
 		name += ':'; //eslint-disable-line
 		for (const i in this.states) {
@@ -99,6 +142,10 @@ class Actor {
 		return null;
 	}
 
+	/** Перейти к состоянию
+	 * @param  {string} name - Состояние
+	 * @returns {undefined} ...
+	 */
 	gotoState(name) {
 		this.statePtr = this.findState(name);
 		if (!this.statePtr) {
@@ -108,6 +155,9 @@ class Actor {
 		this.isGoto = true;
 	}
 
+	/** Получить длительность состояния
+	 * @returns {number} Длительность
+	 */
 	getStateTime() {
 		const state = this.states[this.statePtr];
 
@@ -118,6 +168,9 @@ class Actor {
 		return 0;
 	}
 
+	/** Обновить состояние актора
+	 * @param  {number} time - Длительность состаяния
+	 */
 	updateState(time) {
 		if (this.timer) {
 			try {
@@ -133,6 +186,10 @@ class Actor {
 		}
 	}
 
+	/** Тик
+	 * @private
+	 * @returns {undefined} ...
+	 */
 	tick() {
 		const state = this.states[this.statePtr];
 
@@ -175,6 +232,10 @@ class Actor {
 		this.updateState(time);
 	}
 
+	/** Задать спрайт
+	 * @param  {sting} name - Имя спрайта без расширения (.png)
+	 * @returns {undefined} ...
+	 */
 	setSprite(name) {
 		let el = this.element;
 
@@ -190,6 +251,20 @@ class Actor {
 		el.src = `res/${this.spriteFolder}/${name}.png`;
 	}
 
+	/**
+	 * Проверяет на столкновение
+	 * @param {object} object - Объект
+	 * @returns {bool} True/false
+	 */
+	detectCollision(/* object */){
+		//Actor.calculateDistance();
+		return this;
+	}
+
+	/** Передвигает актора в направлении direction со скоростью speed
+	 * @param  {number} direction - Направление
+	 * @param  {number} speed - Спид
+	 */
 	move(direction, speed) {
 		let xofs = speed * Math.sin(direction * Math.PI / 180);
 		let yofs = -(speed * Math.cos(direction * Math.PI / 180));
@@ -199,14 +274,29 @@ class Actor {
 	}
 
 	//region static
+	/** Создает HTML img элемент
+	 * @private
+	 * @static
+	 * @returns {Image} Элемент
+	 */
 	static createActorElement() {
 		return document.createElement('img');
 	}
 
+	/** Развернуть направление на 180 градусов
+	 * @static
+	 * @param  {number} angle - Направление в градусах
+	 * @returns {number} - id направления
+	 */
 	static reverseDirection(angle) {
 		return this.angle2direction(180 - angle);
 	}
 
+	/** Перевести градусы в id
+	 * @static
+	 * @param  {number} angle - Направление в градусах
+	 * @returns {number} - id направления
+	 */
 	static angle2direction(angle) {
 		if (angle >= -22.5 && angle <= 22.5) {
 			return 5;
@@ -237,6 +327,12 @@ class Actor {
 		return 1;
 	}
 
+	/** Рассчитать направление в сторону актора
+	 * @static
+	 * @param  {Actor} actor - Актор, относительно которого расчитывается направление
+	 * @param  {Actor} target - Актор, в сторону которого расчитывается
+	 * @returns {object} {angle, direction} - Направление в градусах и id
+	 */
 	static calculateDirection(actor, target) {
 		//На опережение
 		let angle = Math.atan2(target.x - actor.x, -(target.y - actor.y)) * 180.0 / Math.PI;
@@ -253,33 +349,35 @@ class Actor {
 		// return angle;
 	}
 
+	/** Рассчитать растояние до объекта
+	 * @static
+	 * @param  {Actor} actor - Актор, относительно которого расчитывается растояние
+	 * @param  {Actor} target - Актор, до которого расчитывается растояние
+	 * @returns {number} Растояние
+	 */
 	static calculateDistance(actor, target) {
 		//Модуль вектора this -> actor
 		return Math.sqrt(Math.pow(target.x - actor.x, 2) + Math.pow(target.y - actor.y, 2));
 	}
 
+	/** Получить имя спрайта по номеру анимации и направления
+	 * @static
+	 * @param  {string} name - Префикс спрайта (4 буквы)
+	 * @param  {number} animation=1 - Номер анимации
+	 * @param  {number} direction=1 - Номер направления
+	 * @returns {string} Название спрайта без расширения
+	 */
 	static getSpriteName(name, animation = 1, direction = 1) {
 		const letters = [null,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o'];
 
 		return name + letters[animation] + direction;
 	}
-
-	static get IDLE(){
-		return 'IDLE'; //TODO: Сделать объекты с разными свойствами
-	}
-
-	static get SHOOTING(){
-		return 'SHOOTING';
-	}
-
-	static get WALKING(){
-		return 'WALKING';
-	}
-
-	static get DIED(){
-		return 'DIED';
-	}
 	//endregion static
 }
+
+Actor.IDLE = 'IDLE'; //TODO: Сделать объекты с разными свойствами
+Actor.SHOOTING = 'SHOOTING';
+Actor.WALKING = 'WALKING';
+Actor.DIED = 'DIED';
 
 // module.exports = Actor;
